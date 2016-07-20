54c2e1fb5e44878f6e08541ad48ecb431bf2d3db
Vasil <v.mitov.clouway@gmail.com>
2016-07-20 17:47:28 +0300

    Some changes were made to task3.

--- src/com/clouway/task3/Demo.java
+++ src/com/clouway/task3/Demo.java
@@ -9,8 +9,6 @@ public class Demo {
     SynchronyzedThread counter = new SynchronyzedThread(1, 10);
     Thread firstThread = new Thread(counter);
     Thread secondThread = new Thread(counter);
-    counter.informThread(secondThread);
-    counter.informThread(secondThread);
     firstThread.start();
     secondThread.start();
   }
--- src/com/clouway/task3/SynchronyzedThread.java
+++ src/com/clouway/task3/SynchronyzedThread.java
@@ -6,15 +6,10 @@ package com.clouway.task3;
 public final class SynchronyzedThread implements Runnable {
   private Integer from;
   private Integer to;
-  private Thread otherThread;
 
   public SynchronyzedThread(Integer from, Integer to) {
     this.from = from;
     this.to = to;
-  }
-
-  public void informThread(Thread otherThread) {
-    this.otherThread = otherThread;
   }
 
   @Override
@@ -31,6 +26,7 @@ public final class SynchronyzedThread implements Runnable {
           e.printStackTrace();
         }
       }
+      notifyAll();
     }
   }
 }
