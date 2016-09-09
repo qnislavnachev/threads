7bc956a3845899bd4cb49b09513204eb99e1ebe0
qnislavnachgev <qnislav.nachev@gmail.com>
2016-09-09 12:46:13 +0300

    Task 3 fixed and ready for review

--- src/main/java/task3/Counter.java
+++ src/main/java/task3/Counter.java
@@ -1,18 +1,26 @@
 package task3;
 
-public class Counter {
+public class Counter implements Runnable {
+    private int countTo;
 
-    public synchronized void count(int countTo, int counter) {
-        while (countTo > counter) {
-            counter++;
-            System.out.println(Thread.currentThread().getName() + ": " + counter);
-            notifyAll();
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                System.out.println("I was interrupted!");
+    public Counter(int countTo) {
+        this.countTo = countTo;
+    }
+
+    public void run() {
+        int counter = 0;
+        synchronized (this) {
+            while (countTo > counter) {
+                counter++;
+                System.out.println(Thread.currentThread().getName() + ": " + counter);
+                notifyAll();
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Error");
+                }
             }
+            notifyAll();
         }
-        notifyAll();
     }
 }
--- src/main/java/task3/Demo.java
+++ src/main/java/task3/Demo.java
@@ -2,9 +2,9 @@ package task3;
 
 public class Demo {
     public static void main(String[] args) {
-        Counter counter = new Counter();
-        Threads thread1 = new Threads(10, counter);
-        Threads thread2 = new Threads(10, counter);
+        Counter counter = new Counter(10);
+        Thread thread1 = new Thread(counter);
+        Thread thread2 = new Thread(counter);
 
         thread1.start();
         thread2.start();
