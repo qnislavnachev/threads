939d44ad843b4d71b3e5f1b7587f507fa2578d62
qnislavnachev <qnislav.nachev@gmail.com>
2016-09-16 03:35:35 +0300

    Updated Demo class

--- src/task5/demo.java
+++ src/task5/demo.java
@@ -1,13 +1,17 @@
 package task5;
 
-/**
- * Created by IaNiTyy on 16.9.2016 г..
- */
 public class demo {
     public static void main(String[] args) {
         ObjectTable table = new ObjectTable();
         TimeoutThread thread = new TimeoutThread(table, 10);
         thread.start();
         table.put("Lemons", 10);
+        table.put("Oranges", 5);
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            System.out.println("Error");
+        }
+        table.get("Oranges");
     }
 }
